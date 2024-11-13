d06f2923b91b92f97703d22c4c2031b5cc8b69d6
Jasan33 <156905772+Jasan33@users.noreply.github.com>
2024-11-13 11:15:06 +0100

    Made an improved list of the most 200 common words insted of randome

--- views.py
+++ views.py
@@ -6,8 +6,33 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template('index.html', a=a(), b=b(), c=c(), d=d(), e=e(), f=f(), g=g(), h=h(), i=i(), j=j(), k=k(), l=l(), timer=timer_countdown())
+    words = select_words()  # Generate a list of words
+    return render_template('index.html', words=" ".join(words))  # Pass as a single string
 
+def select_words():
+    # Select 15 words from category 'a' and 15 from 'b'
+    words = random.sample(word_categories['a'], 45)
+    random.shuffle(words)  # Shuffle words for variety
+    return words
+
+# Word categories
+word_categories = {
+    'a': [
+        "the", "develop", "of", "and", "a", "to", "in", "he", "have", "it", "that", "for", "they", "I", "with", "as", "not", "on", "she",
+        "at", "by", "this", "we", "you", "do", "but", "from", "or", "which", "one", "would", "all", "will", "there", "say", "who",
+        "make", "when", "can", "more", "if", "no", "man", "out", "other", "so", "what", "time", "up", "go", "about", "than", "into",
+        "could", "state", "only", "new", "year", "some", "take", "come", "these", "know", "see", "use", "get", "like", "then", "first",
+        "any", "work", "now", "may", "such", "give", "over", "think", "most", "even", "find", "day", "also", "after", "way", "many",
+        "must", "look", "before", "great", "back", "through", "long", "where", "much", "should", "well", "people", "down", "own", "just",
+        "because", "good", "each", "those", "feel", "seem", "how", "high", "too", "place", "little", "world", "very", "still", "nation",
+        "hand", "old", "life", "tell", "write", "become", "here", "show", "house", "both", "between", "need", "mean", "call", "develop",
+        "under", "last", "right", "move", "thing", "general", "school", "never", "same", "another", "begin", "while", "number", "part",
+        "turn", "real", "leave", "might", "want", "point", "form", "off", "child", "few", "small", "since", "against", "ask", "late",
+        "home", "interest", "large", "person", "end", "open", "public", "follow", "during", "present", "without", "again", "hold",
+        "govern", "around", "possible", "head", "consider", "word", "program", "problem", "however", "lead", "system", "set", "order",
+        "eye", "plan", "run", "keep", "face", "fact", "group", "play", "stand", "increase", "early", "course", "change", "help", "line"
+    ]
+}
 @views.route("/typing")
 def typing():
     return render_template('typing.html')
@@ -19,85 +44,3 @@ def timer():
         time.sleep(1)
         countdown -= 1
     return "Time is up!"
-
-def a():
-    return random.choice([
-        "apple", "orange", "garbage", "folder", "cry", "cheese", "gorilla", "kitten", 
-        "mountain", "river", "pencil", "dragon", "spaceship", "cloud", "tiger", "guitar", 
-        "butterfly", "sunshine", "laptop", "pizza", "wizard", "bicycle", "ocean", "castle", 
-        "robot", "moon", "penguin", "jungle", "diamond", "carrot", "volcano", "whale", "zebra"
-    ])
-
-def b():
-    return random.choice([
-        "develop", "it", "should", "shoulders", "arms", "chest", "nack", "if", "oh yea", 
-        "legs", "knees", "elbows", "hands", "head", "toes", "ankles", "hips", "eyes", "ears", "boots", "butt",
-    ])
-
-def c():
-    return random.choice([
-        "fish", "compare", "black indian", "Norwegian", "english", "french", "swedish", "malyan", 
-        "german", "chinese", "japanese", "spanish", "portuguese", "korean", "russian", 
-        "brazilian", "dutch", "greek"
-    ])
-
-def d():
-    return random.choice([
-        "will", "why", "but", "ore", "try", "only", "when", "time", "at", "nor", 
-        "and", "though", "where", "if", "whether", "while", "before", "after"
-    ])
-
-def e():
-    return random.choice([
-        "freak", "I hate", "kill", "black", "indian", "purple", "curry eater", "dead", "eat", "deepfry", "murder", "harm", 
-        "slap", "fry", "not", "weak", "small", "thiny", "downfall of", "steal", "bark at", "knock out",
-    ])
-
-def f():
-    return random.choice([
-        "jasan drill", "jasan dop", "deep", "jasan dracula", "jasan drift", "jasan drit", "jasan drop", "jasan dream", "jasan drive", 
-        "jasan dance", "jasan draw", "jasan dig", "jasan deliver", "jasan demand", "jasan deny", "jasan destroyer", "jasan design"
-    ])
-
-def g():
-    return random.choice([
-        "hole", "hollow", "honey", "honeybee", "hippo", "hollowed", "heart", "helmet", 
-        "house", "hamster", "hat", "hill", "holiday", "hope", "horizon", "horn", "hospital"
-    ])
-
-def h():
-    return random.choice([
-        "is", "are", "have", "has", "were", "was", "had", "have", "is", "are", 
-        "seems", "looks", "appears", "becomes", "remains", "feels", "turns"
-    ])
-
-def i():
-    return random.choice([
-        "better", "worse", "more", "less", "more", "less", "even", "fewer", 
-        "stronger", "weaker", "greater", "smaller", "taller", "lower", "higher", "deeper"
-    ])
-
-def j():
-    return random.choice([
-        "red", "black", "blue", "orange", "yellow", 
-        "green", "purple", "white", "pink", "grey", "silver", "gold", "brown", "violet"
-    ])
-
-def k():
-    return random.choice([
-        "so", "then", "because", "as", "as", "as", "than", "since", "until", "while", 
-        "although", "though", "if", "unless", "before", "after", "when", "whenever"
-    ])
-
-def l():
-    return random.choice([
-        "rare", "common", "uncommon", "legendary", "mytical", "star", "moon", "sun", "epic", "classified", 
-        "corrupt", "divine", "ancor", "seen", "better", "gloryfied", "something"
-    ])
-
-def timer_countdown():
-    countdown = 30
-    while countdown >= 0:
-        yield countdown
-        time.sleep(1)
-        countdown -= 1
